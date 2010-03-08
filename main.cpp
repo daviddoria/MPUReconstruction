4a04a3c240064b7b792e11c9e0d570164f5caa65
Arnaud GELAS <arnaud_gelas@hms.harvard.edu>
2010-03-08 10:46:27 -0500

    ENH: Add level as arugments in main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,15 @@
 
 int main( int argc, char *argv[])
 {
+  if( argc != 4 )
+    {
+    std::cerr <<"MPU(.exe) takes 3 arguments" <<std::endl;
+    std::cerr <<"1-Input filename" <<std::endl;
+    std::cerr <<"2-Level" <<std::endl;
+    std::cerr <<"3-Output filename" <<std::endl;
+    return EXIT_FAILURE;
+    }
+
 	vtkSmartPointer<vtkXMLPolyDataReader> reader = 
       vtkSmartPointer<vtkXMLPolyDataReader>::New();
   reader->SetFileName(argv[1]);
@@ -14,11 +23,12 @@ int main( int argc, char *argv[])
   vtkSmartPointer<vtkMPU> mpu = 
       vtkSmartPointer<vtkMPU>::New();
   mpu->SetInputConnection(reader->GetOutputPort());
+  mpu->SetLevel( atoi( argv[2] ) );
   mpu->Update();
   
   vtkSmartPointer<vtkXMLPolyDataWriter> writer = 
       vtkSmartPointer<vtkXMLPolyDataWriter>::New();
-  writer->SetFileName(argv[2]);
+  writer->SetFileName( argv[3] );
   writer->SetInputConnection(mpu->GetOutputPort());
   writer->Write();
   
